46e4ef215c6bb8fa434a03106a49a7da516f545c
Wagner Pedro <wagnerpedro355@gmail.com>
2023-05-22 22:39:07 -0700

    arquitectura BEM nav

--- assets/Js/menu.js
+++ assets/Js/menu.js
@@ -1,11 +1,11 @@
 const btnMenu = document.querySelector('.menu');
 const menu = document.querySelector('.menu > img')
-const ul = document.querySelector('nav > .nav-container ul');
-const li = document.querySelectorAll('nav > .nav-container ul li')
+const ul = document.querySelector('.nav__container__ul');
+const li = document.querySelectorAll('.nav__container__ul li')
 
 
 btnMenu.addEventListener('click', ()=> {
-    ul.classList.toggle('menuShow')
+    ul.classList.toggle('nav__container__ul--menushow')
     li.forEach((el)=> {
     el.classList.toggle('slideLi')
     })
